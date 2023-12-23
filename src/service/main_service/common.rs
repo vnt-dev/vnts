42a646ef9ce9b84baf34938dd945f74b8b28107b
lubeilin <1791778603@qq.com>
2023-12-24 01:21:17 +0800

    更新版本号

--- src/service/main_service/common.rs
+++ src/service/main_service/common.rs
@@ -435,7 +435,7 @@ async fn server_packet_pre_handle(
                 service_packet::Protocol::HandshakeRequest => {
                     // 握手请求,有加密的话回应公钥
                     let mut res = message::HandshakeResponse::new();
-                    res.version = "1.1.3".to_string();
+                    res.version = "1.2.8".to_string();
                     if let Some(rsp_cipher) = rsa_cipher {
                         res.public_key.extend_from_slice(rsp_cipher.public_key());
                         res.secret = true;
